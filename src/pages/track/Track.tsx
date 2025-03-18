13f1ec392f73e0b5e99d4c4b2b6c165e08b41e07
uday <udaysubba2004@gmail.com>
2025-03-18 22:19:19 +0530

    add scale and zoom

--- src/pages/track/Track.tsx
+++ src/pages/track/Track.tsx
@@ -1,8 +1,14 @@
+import "./Track.css";
 import Map from 'ol/Map.js';
 import View from 'ol/View.js';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import { useEffect, useRef } from 'react';
+import { fromLonLat } from 'ol/proj';
+
+// Import additional controls and features
+import ScaleLine from 'ol/control/ScaleLine';
+import Zoom from 'ol/control/Zoom';
 
 
 export default function Track(){
@@ -11,16 +17,25 @@ export default function Track(){
 
     useEffect(() => {
         if (mapRef.current) {
+            //Halmira Bridge
+            const startingLocation = fromLonLat([93.94114639182163, 26.51823896692769]);
+            // const startingLocation = fromLonLat([26.6700, 47.4768]);
+
+
 
             //map object
             const map = new Map({
                 layers: [
-                  new TileLayer({source: new OSM()}),
+                    new TileLayer({source: new OSM()}),
                 ],
                 view: new View({
-                  center: [0, 0],
-                  zoom: 2,
+                    center: startingLocation,
+                    zoom: 12,
                 }),
+                controls: [
+                    new ScaleLine(), // Add scale line to the map
+                    new Zoom()
+                ],
                 target: mapRef.current,
               });
     
@@ -31,7 +46,7 @@ export default function Track(){
     return (
         <>
             <h3>Tracking</h3>
-            <div ref={mapRef} className='border w-[100%] h-[400px]'></div>
+            <div ref={mapRef} className='border w-[100%] h-[800px]'></div>
         </>
     )
 }
