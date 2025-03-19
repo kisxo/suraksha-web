0ff5f19e188c46aed891442cc5643570cd272d9b
uday <udaysubba2004@gmail.com>
2025-03-19 23:15:17 +0530

    style map page

--- src/pages/track/Track.tsx
+++ src/pages/track/Track.tsx
@@ -154,16 +154,31 @@ export default function Track(){
     map.render()
 
     return (
-        <>
-            <h3>Tracking</h3>
-            <div ref={mapRef} className='h-[600px]'></div>
+        <div>
+            <div className="flex justify-between content-center py-5 px-4 text-slate-600">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-7 h-7">
+                    <rect width="256" height="256" fill="none"/>
+                    <line x1="216" y1="128" x2="40" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"/>
+                    <polyline points="112 56 40 128 112 200" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"/>
+                </svg>
+                <span className="content-center text-xl font-semibold">Location Tracking</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-7 h-7">
+                    <rect width="256" height="256" fill="none"/>
+                    <circle cx="128" cy="128" r="40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"/>
+                    <path d="M41.43,178.09A99.14,99.14,0,0,1,31.36,153.8l16.78-21a81.59,81.59,0,0,1,0-9.64l-16.77-21a99.43,99.43,0,0,1,10.05-24.3l26.71-3a81,81,0,0,1,6.81-6.81l3-26.7A99.14,99.14,0,0,1,102.2,31.36l21,16.78a81.59,81.59,0,0,1,9.64,0l21-16.77a99.43,99.43,0,0,1,24.3,10.05l3,26.71a81,81,0,0,1,6.81,6.81l26.7,3a99.14,99.14,0,0,1,10.07,24.29l-16.78,21a81.59,81.59,0,0,1,0,9.64l16.77,21a99.43,99.43,0,0,1-10,24.3l-26.71,3a81,81,0,0,1-6.81,6.81l-3,26.7a99.14,99.14,0,0,1-24.29,10.07l-21-16.78a81.59,81.59,0,0,1-9.64,0l-21,16.77a99.43,99.43,0,0,1-24.3-10l-3-26.71a81,81,0,0,1-6.81-6.81Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"/>
+                </svg>
 
-                <h3>Map</h3>
-                <label>
-                    track position
-                    <button onClick={()=> {startTracking()}} className="bg-blue-500">Tract</button>
-                </label>
+            </div>
+            <div className="relative">
+                <div ref={mapRef} className='h-[400px] relative'></div>
+                <div className="bg-white rounded-t-3xl absolute w-full bottom-0 text-center py-5 font-semibold text-xl text-blue-500 border-b">
+                    Live Tracking
+                </div>
+            </div>
+            
+            <div onClick={()=> {startTracking()}} className="bg-blue-500 w-1/3 mx-auto text-center text-white rounded py-2 my-4">Start Tracking</div>
+               
                 
-        </>
+        </div>
     )
 }
